610bbbf8646030b5911df26759effcda11bb524a
ferreirix <ferreira.s.joao@gmail.com>
2020-11-15 12:11:05 +0100

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,16 +8,8 @@ const MenuItems = [
     title: "Home"
   },
   {
-    path: "/about",
-    title: "About"
-  },
-  {
     path: "/blog",
     title: "Blog"
-  },
-  {
-    path: "/contact",
-    title: "Contact"
   },
 ]
 
