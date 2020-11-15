481ad36e2f6ff8cd4202925a93446fc8bcb49c7d
Joao Ferreira <jferreira@nomadvantage.com>
2020-11-15 16:35:48 +0100

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,14 @@
 import React from "react"
 import {Link} from "gatsby"
-import { RiHeart2Line } from "react-icons/ri";
+import { RiFacebookCircleFill } from "react-icons/ri";
 
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>A GatsbyJS Starter for Netlify CMS, Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="/">Stackrole.com</Link></p>
+      <p>Desenvolvido em parceria com <Link to="https://www.vncasainteligente.pt/">VN Casa Inteligente</Link>
+        &emsp; <RiFacebookCircleFill style={{paddingTop: 2}}/> 
+      <Link to="https://www.facebook.com/casascominteligencia/" target="_blank">Facebook</Link>
+      </p>
     </div>
   </footer>
 )
